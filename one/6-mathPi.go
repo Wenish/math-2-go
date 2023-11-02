e265ca6d9ff9060b572b8ad4636df1288faabad9
Wenish <Wenish@users.noreply.github.com>
2023-11-02 23:36:21 +0100

    Update 6-mathPi.go

--- one/6-mathPi.go
+++ one/6-mathPi.go
@@ -9,6 +9,14 @@ func printMathPi() {
 	fmt.Println("-------------------------------------------")
 	fmt.Println("6 math.Pi ̸= π")
 	fmt.Println("math.Pi", math.Pi)
+
+	// 1. Beginne mit einem Intervall [A, B], für welches gilt: π ∈ [A, B]
+	// 2. Falls A = B → Ende!
+	// 3. Aus den Grenzen des Intervalls A und B berechne ein C, so dass A < C < B
+	// 4. Falls π ∈ [A, C] → neues Intervall ist [A, C]
+	// 5. Falls π ∈ [C, B] → neues Intervall ist [C, B]
+	// 6. Gehe zu Punkt 2
+
 	A := 3.0
 	B := 3.2
 
