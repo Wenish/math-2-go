868529d89eb93c8570c9db684ac741073856cb0e
Wenish <Wenish@users.noreply.github.com>
2023-11-25 11:25:40 +0100

    Update 3-wurzeln.go

--- one/3-wurzeln.go
+++ one/3-wurzeln.go
@@ -9,9 +9,7 @@ func printWurzeln() {
 	fmt.Println("-------------------------------------------")
 	fmt.Println("3 Wurzeln")
 
-	var equation bool
-
-	equation = math.Pow(math.Sqrt(2), 2) == 2
+	equation := math.Pow(math.Sqrt(2), 2) == 2
 
 	fmt.Println("Die Gleichung ist in go folgendes:", equation)
 	fmt.Println("-------------------------------------------")
